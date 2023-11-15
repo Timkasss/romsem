ad3a846f2a4314510c45b53a10ec912c338252c0
Timka <timkaanddoge@gmail.com>
2023-11-15 20:30:49 +0200

    HOME page, without adaptiv

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
-import '../style/main.scss'
+import '../style/main.scss';
+import '../style/basket.scss'
 import chiken from '../image/chiken.jpg';
 import pizza from '../image/pizza.jpg';
 import yger from '../image/ygr.jpg';
@@ -8,10 +9,83 @@ import salomon from '../image/salomon.png';
 import filadelfia from '../image/filadelfia.png';
 import themostbigfil from '../image/the_most_big_fil.png';
 
+import AsLogo from '../image/logo.png'
+import AsIcon1 from '../image/pizza.svg'
+import AsIcon2 from '../image/set.svg'
+import AsIcon3 from '../image/wok.svg'
+import AsIcon4 from '../image/rol.svg'
+import AsIcon5 from '../image/syw.svg'
+import AsIcon6 from '../image/salate.svg'
+import AsIcon7 from '../image/soup.svg'
+import AsIcon8 from '../image/corn.svg'
+import AsIcon9 from '../image/drink.svg'
+import AsIcon10 from '../image/action.svg'
+
+
+import Basket from '../components/Basket';
+
 
 function Home() {
+
+   function showeDetails() {
+      const shadow = document.querySelectorAll('.order_details_text');
+      const hidden = document.querySelectorAll('.details-shadow-none');
+      const button = document.querySelector('.order_details_button');
+
+      shadow.forEach(item => {
+         if (item.classList.contains('details-shadow')) {
+            item.classList.remove('details-shadow')
+         }
+      });
+
+      hidden.forEach(item => {
+         item.style.display = "block";
+      })
+      button.style.display = "none"
+   }
+
    return (
       <main className="main">
+         <aside className="aside_menu_wrapper">
+            <div className="aside_logo">
+               <img src={AsLogo} alt="logo" />
+            </div>
+            <nav className="aside_menu">
+               <ul className="aside_menu_list">
+                  <li className="aside_menu_item">
+                     <img src={AsIcon1} alt="icon" />
+                     <a href="#" className="aside_menu_link">Пицца</a></li>
+                  <li className="aside_menu_item">
+                     <img src={AsIcon2} alt="icon" />
+                     <a href="#" className="aside_menu_link">Сеты</a></li>
+                  <li className="aside_menu_item">
+                     <img src={AsIcon3} alt="icon" />
+                     <a href="#" className="aside_menu_link">WOK</a></li>
+                  <li className="aside_menu_item">
+                     <img src={AsIcon4} alt="icon" />
+                     <a href="#" className="aside_menu_link">Роллы</a></li>
+                  <li className="aside_menu_item">
+                     <img src={AsIcon5} alt="icon" />
+                     <a href="#" className="aside_menu_link">Суши</a></li>
+                  <li className="aside_menu_item">
+                     <img src={AsIcon6} alt="icon" />
+                     <a href="#" className="aside_menu_link">Салаты</a></li>
+                  <li className="aside_menu_item">
+                     <img src={AsIcon7} alt="icon" />
+                     <a href="#" className="aside_menu_link">Супы</a></li>
+                  <li className="aside_menu_item">
+                     <img src={AsIcon8} alt="icon" />
+                     <a href="#" className="aside_menu_link">Корн доги</a></li>
+                  <li className="aside_menu_item">
+                     <img src={AsIcon9} alt="icon" />
+                     <a href="#" className="aside_menu_link">Напитки</a></li>
+                  <li className="aside_menu_item">
+                     <img src={AsIcon10} alt="icon" />
+                     <a href="#" className="aside_menu_link">Акции</a></li>
+               </ul>
+            </nav>
+         </aside>
+         <Basket />
          <div className="main__container">
             <div className="main_content">
                <div className="main__min_container">
@@ -70,6 +144,20 @@ function Home() {
                         <div className="popular_popular swith_hov">Popular</div>
                      </div>
                      <div className="slider_food_items">
+
+                        <div className="slider_controller_pre">
+                           <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60" fill="none">
+                              <ellipse cx="30" cy="30" rx="30" ry="30" transform="rotate(90 30 30)" fill="#111111" />
+                              <path d="M36 15L23 28.5L36 42" stroke="#F2F2F2" strokeLinecap="round" />
+                           </svg>
+                        </div>
+                        <div className="slider_controller_next">
+                           <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60" fill="none">
+                              <ellipse cx="30" cy="30" rx="30" ry="30" transform="rotate(90 30 30)" fill="#111111" />
+                              <path d="M23 15L36 28.5L23 42" stroke="#F2F2F2" strokeLinecap="round" />
+                           </svg>
+                        </div>
+
                         <div className="slider_food_item">
                            <div className="slider_food_item_img">
                               <img src={salomon} alt="food" />
@@ -128,7 +216,7 @@ function Home() {
                      <p className="order_details_text details-shadow-none"> В меню также представлены гунканы: с начинкой из красной икры и тобико, а также феликсы, где японский майонез сочетается с рыбой, морепродуктами, угрем. Любители острых блюд могут купить суши с соусом спайси. Популярные начинки — копченая курица, снежный краб, креветки, гребешки, тунец, лосось и окунь.</p>
 
 
-                     <div className="order_details_button">
+                     <div onClick={showeDetails} className="order_details_button">
                         Подробнее
                         <svg width="17" height="9" viewBox="0 0 17 9" fill="none" xmlns="http://www.w3.org/2000/svg">
                            <path d="M16 1L8.5 8L1 0.999999" stroke="#F46D40" strokeLinecap="round" />
